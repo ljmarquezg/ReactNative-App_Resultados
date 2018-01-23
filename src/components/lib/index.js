4db60bf5e085687049a04e3ae09000b0a9005ad2
Luis Jose Marquez Gonzalez <ljmarquezg@gmail.com>
2018-01-23 10:20:41 -0400

    Initial commit

---
+++ src/components/lib/index.js
@@ -0,0 +1,8 @@
+export * from './Encabezado';
+export * from './BarraInferior';
+export * from './Card';
+export * from './CardSection';
+export * from './Boton';
+export * from './Input';
+export * from './Spinner';
+
